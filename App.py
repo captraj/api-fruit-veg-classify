042962754a393bfd8d86d976dc7b32251aad4db9
captraj <76588598+captraj@users.noreply.github.com>
2023-06-22 22:30:35 +0530

    added data preprocessing and class labels for classification

--- App.py
+++ App.py
@@ -15,3 +15,16 @@ num_classes = 36  # Update with the number of classes in your model
 model.classifier[1] = nn.Linear(model.last_channel, num_classes)
 model.load_state_dict(torch.load('modelforclass.pth', map_location=torch.device('cpu')))
 model.eval()
+
+# Define the data transformations
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+class_labels = ['apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot', 'cauliflower',
+                'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi',
+                'lemon', 'lettuce', 'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 'pineapple', 'pomegranate',
+                'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato', 'turnip',
+                'watermelon']
